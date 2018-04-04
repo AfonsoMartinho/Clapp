ff4e570f4aa14d1f43b8de7dc7c8a10a9c8bebf4
ruigomespt <RuiGomesPT43@gmail.com>
2018-04-04 12:00:38 +0100

    image fix

--- app/src/main/java/com/project/clapp/CreateEvent.java
+++ app/src/main/java/com/project/clapp/CreateEvent.java
@@ -8,13 +8,17 @@ import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -71,6 +75,7 @@ public class CreateEvent extends AppCompatActivity implements NumberPicker.OnVal
     private static final String TAG = "Check";
     private static final int ERROR_DIALOG_REQUEST = 9001;
     private static final int REQUEST_CAMERA = 1, SELECT_FILE = 2, REQUEST_LOCATION = 3;
+    public static final int MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE = 123;
     private FirebaseAuth mAuth;
 
 
@@ -314,49 +319,54 @@ public class CreateEvent extends AppCompatActivity implements NumberPicker.OnVal
     }
 
     public void addImage(View view) {
-        final CharSequence[] items = { "Take Photo", "Choose from Library",
-                "Cancel" };
-        AlertDialog.Builder builder = new AlertDialog.Builder(CreateEvent.this);
-        builder.setTitle("Add Photo!");
-        builder.setItems(items, new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int item) {
-                if (items[item].equals("Take Photo")) {
-                    Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    startActivityForResult(intent, REQUEST_CAMERA);
-                } else if (items[item].equals("Choose from Library")) {
-                    Intent intent = new Intent(Intent.ACTION_PICK,  MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                    intent.setType("images/*");
-                    startActivityForResult(intent.createChooser(intent, "Select File"), SELECT_FILE);
-                } else if (items[item].equals("Cancel")) {
-                    dialog.dismiss();
+
+            final CharSequence[] items = { "Take Photo", "Choose from Library",
+                    "Cancel" };
+            AlertDialog.Builder builder = new AlertDialog.Builder(CreateEvent.this);
+            builder.setTitle("Add Photo!");
+            builder.setItems(items, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int item) {
+                    if (items[item].equals("Take Photo")) {
+                        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                        startActivityForResult(intent, REQUEST_CAMERA);
+                    } else if (items[item].equals("Choose from Library")) {
+                        Intent intent = new Intent(Intent.ACTION_PICK,  MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                        intent.setType("images/*");
+                        startActivityForResult(intent.createChooser(intent, "Select File"), SELECT_FILE);
+                    } else if (items[item].equals("Cancel")) {
+                        dialog.dismiss();
+                    }
                 }
-            }
-        });
-        builder.show();
+            });
+            builder.show();
+
     }
 
 
 
     public void createEvent(View view) {
 
+
         name = nameTxt.getText().toString();
-        saveImage(getImageUri(this, bmp));
-        EventFirebaseManager efm = EventFirebaseManager.getInstance();
-        efm.addEvent(
-                name,
-                date,
-                time,
-                local,
-                duration,
-                preco,
-                desc,
-                limit,
-                userId,
-                latitude,
-                longitude,
-                imgURL,
-                tags);
+        if (checkPermissionREAD_EXTERNAL_STORAGE(this)) {
+            saveImage(getImageUri(this, bmp));
+            EventFirebaseManager efm = EventFirebaseManager.getInstance();
+            efm.addEvent(
+                    name,
+                    date,
+                    time,
+                    local,
+                    duration,
+                    preco,
+                    desc,
+                    limit,
+                    userId,
+                    latitude,
+                    longitude,
+                    imgURL,
+                    tags);
+        }
     }
 
     //MAP
@@ -388,6 +398,68 @@ public class CreateEvent extends AppCompatActivity implements NumberPicker.OnVal
         }
         return false;
 
+    }
+    public boolean checkPermissionREAD_EXTERNAL_STORAGE(
+            final Context context) {
+        int currentAPIVersion = Build.VERSION.SDK_INT;
+        if (currentAPIVersion >= android.os.Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(context,
+                    android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                if (ActivityCompat.shouldShowRequestPermissionRationale(
+                        (Activity) context,
+                        android.Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    showDialog("External storage", context,
+                            android.Manifest.permission.READ_EXTERNAL_STORAGE);
+
+                } else {
+                    ActivityCompat
+                            .requestPermissions(
+                                    (Activity) context,
+                                    new String[] {android.Manifest.permission.READ_EXTERNAL_STORAGE},
+                                    MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                }
+                return false;
+            } else {
+                return true;
+            }
+
+        } else {
+            return true;
+        }
+    }
+    public void showDialog(final String msg, final Context context,
+                           final String permission) {
+        AlertDialog.Builder alertBuilder = new AlertDialog.Builder(context);
+        alertBuilder.setCancelable(true);
+        alertBuilder.setTitle("Permission necessary");
+        alertBuilder.setMessage(msg + " permission is necessary");
+        alertBuilder.setPositiveButton(android.R.string.yes,
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        ActivityCompat.requestPermissions((Activity) context,
+                                new String[] { permission },
+                                MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE);
+                    }
+                });
+        AlertDialog alert = alertBuilder.create();
+        alert.show();
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // do your stuff
+                } else {
+                    Toast.makeText(CreateEvent.this, "GET_ACCOUNTS Denied",
+                            Toast.LENGTH_SHORT).show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions,
+                        grantResults);
+        }
     }
 
 
